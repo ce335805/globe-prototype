dea928d53b13bedada0137c6bac7a39ca39b5c60
ce335805 <christian.eckhardt@rwth-aachen.de>
2021-09-05 19:46:22 +0200

    Turning smooth now
    merge geometries into one for performance

--- src/components/Globe.vue
+++ src/components/Globe.vue
@@ -8,6 +8,8 @@
 <script>
 import * as Three from 'three'
 import {DEG2RAD} from "three/src/math/MathUtils";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {BufferGeometryUtils} from "three/examples/jsm/utils/BufferGeometryUtils";
 
 export default {
   name: 'globe',
@@ -16,12 +18,16 @@ export default {
       camera: null,
       scene: null,
       renderer: null,
+      controls: null,
       globe: null,
+      testCircle: null,
       rows: 160,
       dotDensity: 80,
       imgData: null,
       imgWidth: 0,
-      imgHeight: 0
+      imgHeight: 0,
+      circleArr: null,
+      singleCircle: null
     }
   },
   methods: {
@@ -29,54 +35,69 @@ export default {
       let container = document.getElementById('container');
 
       this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.005, 10);
-      this.camera.position.z = 2;
+      this.camera.position.z = 2.4;
 
       this.scene = new Three.Scene();
 
       let geometry = new Three.SphereGeometry( 1, 64, 64 );
-      let material = new Three.MeshBasicMaterial({color: 0x1177ff});
+      let material = new Three.MeshBasicMaterial({color: 0x3251a6});
       this.globe = new Three.Mesh(geometry, material);
-      this.scene.add(this.globe);
+      //this.scene.add(this.globe);
 
-      this.renderer = new Three.WebGLRenderer({antialias: true});
+      //some stuff for performance optimization
+      let pixelRatio = window.devicePixelRatio
+      let AA = true
+      if (pixelRatio > 1) {
+        AA = false
+      }
+      this.renderer = new Three.WebGLRenderer({
+        antialias: AA,
+        powerPreference: "high-performance",
+      });
       this.renderer.setSize(container.clientWidth, container.clientHeight);
       container.appendChild(this.renderer.domElement);
+
+      this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+      this.controls.autoRotate = true;
+      this.controls.update();
 
     },
     animate: function() {
       requestAnimationFrame(this.animate);
-      this.globe.rotation.x += 0.0;
-      this.globe.rotation.y += 0.0;
+      this.controls.update();
+      //this.singleCircle.position.x += 0.1;
       this.renderer.render(this.scene, this.camera);
     },
     drawCircles: function() {
-      let geometryCircle = new Three.CircleGeometry( 0.005, 5);
-      let materialCircle = new Three.MeshBasicMaterial({color: 0xffffff});
-
+      let geometries = [];
       for (let lat = -90; lat <=90; lat += 180/this.rows){
         const radius = Math.cos(Math.abs(lat) * DEG2RAD);
         const circumference = radius * Math.PI * 2;
         const dotsForLat = Math.floor(circumference * this.dotDensity);
         for(let x = 0; x < dotsForLat; x += 1){
           const long = x * 360./dotsForLat;
-          if(lat < -50 || lat > 50) {
+          if (!this.visibilityForCoordinate(long, lat) || (lat < -60 || lat > 60)){
             continue;
           }
-          if (!this.visibilityForCoordinate(long, lat)){
-            continue;
-          }
-          let circle = new Three.Mesh(geometryCircle, materialCircle);
-          this.scene.add(circle);
           let xCoord = Math.sin(long * DEG2RAD) * Math.cos(lat * DEG2RAD) * 1.001;
           let yCoord = Math.sin(lat * DEG2RAD) * 1.001;
           let zCoord = Math.cos(long * DEG2RAD) * Math.cos(lat * DEG2RAD) * 1.001;
-          circle.position.set(xCoord, yCoord, zCoord);
+          const m = new Three.Matrix4();
+          m.makeTranslation(xCoord, yCoord, zCoord);
+          let geometryCircle = new Three.CircleBufferGeometry( 0.005, 3);
+          geometryCircle.applyMatrix4(m);
+          geometries.push(geometryCircle);
         }
       }
+      const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries, false);
+      const materialCircle = new Three.MeshBasicMaterial({color: 0xffffff});
+      materialCircle.side = Three.DoubleSide;
+      const mesh = new Three.Mesh(mergedGeometry, materialCircle);
+      this.scene.add(mesh);
+
     },
     loadWorldMap: function(){
-      //let ctx = document.createElement('canvas').getContext('2d');
-      let cnvs = document.getElementById('drawWorldMap')
+      let cnvs = document.createElement('canvas')
       let ctx = cnvs.getContext('2d');
       let img = new Image();
       let vm = this;
@@ -88,17 +109,15 @@ export default {
         vm.imgData = ctx.getImageData(0, 0, img.width, img.height);
         vm.imgWidth = img.width;
         vm.imgHeight = img.height;
-        console.log(img.width);
-        console.log(img.height);
         vm.drawCircles();
       };
       img.src = require("../assets/worldmap.png");
 
     },
     visibilityForCoordinate: function(long, lat) {
-      const pixelRow = Math.floor(this.imgHeight/100 * (-lat + 50));
-      const pixelColumn = Math.floor(this.imgWidth/360 * ((long + 90) % 360));
-      return this.imgData.data[pixelRow * this.imgWidth * 4 + pixelColumn * 4 + 3] > 0;
+      const pixelRow = Math.floor(this.imgHeight/120 * (-lat + 60));
+      const pixelColumn = Math.floor(this.imgWidth/360 * ((long + 180) % 360));
+      return this.imgData.data[pixelRow * this.imgWidth * 4 + pixelColumn * 4 + 3] > 30;
     }
   },
   mounted() {
